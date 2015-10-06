0e57d3ff4d819dff6d99d1034dae3852cf964cc0
Sorenfu <sh@compfix.dk>
2015-10-06 15:22:07 +0200

    Setting vars back

--- lib/services/food_list_handler.rb
+++ lib/services/food_list_handler.rb
@@ -3,7 +3,7 @@ require 'net/http'
 module Services
   class FoodListHandler
     FOOD_LIST_URL = 'http://www.aarstiderne.com/Services/ItemWebService.asmx/GetBuildOfMaterial'
-    RECIPE_URL = 'http://www.aarstiderne.com/Opskrifter/Menuoversigt?&persons=2&days=3&d='
+    RECIPE_URL = 'http://www.aarstiderne.com/Opskrifter/Menuoversigt?&persons=2&days=5&d='
 
     def get_list_for_week(date)
       food_items = get_food_items date
@@ -24,7 +24,7 @@ module Services
     def get_food_items(date)
       uri = URI(FOOD_LIST_URL)
       Rails.logger.info "Getting url: #{uri}"
-      res = Net::HTTP.post_form(uri, itemNo: 115064, shipmentDate: date)
+      res = Net::HTTP.post_form(uri, itemNo: 115034, shipmentDate: date)
       raw_json = res.body.scan(/<string .*>(.*)<\/string>/).flatten.last
       json = JSON.load raw_json
       json['Components'].map { |x| {name: x['Name'], quantity: x['Quantity'], unit: x['UnitOfMeasure']} }
@@ -35,7 +35,7 @@ module Services
       Rails.logger.info "Getting url: #{uri}"
       res = Net::HTTP.get(uri)
       doc = Nokogiri::HTML res
-      meals = doc.xpath('//div[@data-family-type="LowCarb" and @data-days="3" and @data-persons="2"][1]//div[@class="meals"]/a')
+      meals = doc.xpath('//div[@data-family-type="LowCarb" and @data-days="5" and @data-persons="2"][1]//div[@class="meals"]/a')
       meals.map { |x| {name: x.text, url: x.xpath('.//@href').text} }
     end
 
