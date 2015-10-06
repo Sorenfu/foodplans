2bb4e84597370eaa86b894ea862b24aaa1e2dd6a
Sorenfu <sh@compfix.dk>
2015-10-06 15:17:32 +0200

    Div fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150831171100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "food_items", force: true do |t|
     t.string   "name"
     t.float    "quantity"
